13be23ed1fca65031b5b42098a03ce1d08c468e4
Casey Schneider-Mizell <caseysm@gmail.com>
2018-10-18 11:08:18 -0700

    fixed installation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 with open(path.join(here, 'requirements.txt'), 'r') as f:
-    requirements = f.read()
+    requirements = f.read().splitlines()
 
 setup(
     name='catalysis',
@@ -20,8 +20,9 @@ setup(
     url='https://github.com/ceesem/catalysis',
     author='Casey Schneider-Mizell',
     author_email='caseysm@gmail.com',
-    packages=find_packages(),
+    packages=['catalysis'],
     install_requires=requirements,
+    include_package_data=True,
     data_files=[('data', ['data/Brain_Lineage_Landmarks_EMtoEM_ProjectSpace.csv',
                           'data/example_project_info.json',
                           'smat_jefferis.csv'])],
